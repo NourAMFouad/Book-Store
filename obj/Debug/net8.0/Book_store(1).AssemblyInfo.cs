014909175200d12a6849d60851d5c896feb292cc
NourAMFouad <nouranahmedfouad141@gmail.com>
2024-05-27 13:22:13 +0300

    update1

--- obj/Debug/net8.0/Book_store(1).AssemblyInfo.cs
+++ obj/Debug/net8.0/Book_store(1).AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Book_store(1)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e735a3672e834aaab9780617630346996615fe8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fb6e468419c662f79499f54cc3263626306be7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Book_store(1)")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Book_store(1)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
